d59c48b9e63f1e26faa4326e2f528a6d0c21f2f0
Amrita Suresh <suresh.am@husky.neu.edu>
2020-03-30 14:54:31 -0400

    experimenting with saved chat functionality
    Weird errors with saving chat in script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // This is going to be the script for the client side
+//const socket = io('35.208.221.63:3000')
 const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
+var chat_transcript2 = []
 
 const name = prompt('Hello! What is your name?')
 appendMsg('')
@@ -10,16 +12,20 @@ socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
     appendMsg(`${data.name}: ${data.message}`)
+    //chat_transcript2.push(`${data.name}: ${data.message}`)
 })
 
 // handling connecting users
 socket.on('user-connected', name => {
     appendMsg(`${name} just joined you!`)
+    chat_transcript2.push(new Date())
 })
 
 // handling disconnects
 socket.on('user-disconnected', name => {
     appendMsg(`${name} has just left the chat`)
+    console.log(chat_transcript2)
+    localStorage.setItem("chat_transcript2", chat_transcript2)
 })
 
 // if we didn't do this, we'd lose all the chat messages
@@ -27,6 +33,7 @@ socket.on('user-disconnected', name => {
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
+    chat_transcript2.push(message)
     // emit sends info from the client to the server
     appendMsg(`You: ${message}`)
     socket.emit('send-chat-message', message)
